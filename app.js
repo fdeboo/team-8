32013d958217b30d80e1435e0646a726b926a124
Frances DeBoo <frances.deboo@gmail.com>
2022-01-29 20:09:54 +0000

    login route implemented

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const path = require("path");
+const User = require("./models/userModel");
 
 const app = express();
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// ROUTES:
+app.post("/api/v1/signup", async (req, res) => {
+  console.log(req.body);
+  try {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name!"],
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: [true, "Please provide your email address"],
+    unique: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
+  },
+});
+
+// INSTANCE METHODS: Available on all documents
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  // returns true or false
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
