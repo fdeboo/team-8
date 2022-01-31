04e9a01894e31feb9c06a674499cdf98f0cce113
Frances DeBoo <frances.deboo@gmail.com>
2022-01-31 00:37:25 +0000

    refactored code

--- public/js/index.js
+++ public/js/index.js
@@ -59,18 +59,58 @@ const signup = async (userData) => {
   }
 };
 
-document.querySelector(".login").addEventListener("click", () => {
-  console.log("clicked Login");
-  login("hello@fran.io", "password1357");
-});
+const loginForm = document.querySelector(".form--login");
+const signupForm = document.querySelector(".form--signup");
+const workleForm = document.querySelector(".form--workle");
+console.log(workleForm);
 
-document.querySelector(".signup").addEventListener("click", () => {
-  console.log("clicked Signup");
-  const userData = {
-    name: "Scooby",
-    email: "melvindoo@mysteryinc.io",
-    password: "password9389",
-    passwordConfirm: "password9389",
-  };
-  signup(userData);
-});
+if (loginForm) {
+  loginForm.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    login(email, password);
+  });
+}
+
+if (signupForm) {
+  signupForm.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    login(email, password);
+  });
+}
+
+if (workleForm) {
+  workleForm.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    const radioInputs = document.getElementsByName("emotion");
+    let selectedEmotion;
+    console.log(radioInputs);
+
+    for (let i = 0; i < radioInputs.length; i++) {
+      console.log(radioInputs[i]);
+      if (radioInputs[i].checked) {
+        selectedEmotion = radioInputs[i];
+      }
+    }
+
+    console.log(selectedEmotion);
+
+    const emotion = document.getElementById("email").value;
+    const comment = document.getElementById("comment").value;
+    // login(email, comment);
+  });
+}
+
+// document.querySelector(".signup").addEventListener("click", () => {
+//   console.log("clicked Signup");
+//   const userData = {
+//     name: "Scooby",
+//     email: "melvindoo@mysteryinc.io",
+//     password: "password9389",
+//     passwordConfirm: "password9389",
+//   };
+//   signup(userData);
+// });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,6 +8,6 @@ router.get("/", authController.isLoggedIn, viewController.getOverview);
 router.get("/me", authController.protect, viewController.getUserHome);
 router.get("/login", authController.isLoggedIn, viewController.getLoginForm);
 router.get("/signup", authController.isLoggedIn, viewController.getSignupForm);
-router.get("/advice", authController.isLoggedIn, viewController.getAdvice);
+router.get("/advice", viewController.getAdvice);
 
 module.exports = router;
