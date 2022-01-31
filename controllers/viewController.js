e56947cc8b611c10d9b061cc9e72fb79b17ad6f0
Frances DeBoo <frances.deboo@gmail.com>
2022-01-31 23:27:51 +0000

    added logout button

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,26 +1,18 @@
 const path = require("path");
 
 exports.getOverview = async (req, res, next) => {
-  console.log("WELCOME HOME");
-
   res.status(200).render("overview", {});
 };
 
 exports.getLoginForm = async (req, res, next) => {
-  console.log("LOGIN PLEASE");
-  console.log("HELLO WORLD");
-
   res.status(200).render("login", {});
 };
 
 exports.getSignupForm = async (req, res, next) => {
-  console.log("SIGNIN PLEASE");
-
   res.status(200).render("signup", {});
 };
 
 exports.getUserHome = async (req, res, next) => {
-  console.log(req.user);
   res.status(200).render("workle", {});
 };
 
